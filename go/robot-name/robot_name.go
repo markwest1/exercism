aa11ec463dd8292db8a132bfd3a7e4765e1e500f
markwest1 <mark.west1@gmail.com>
2017-01-18 23:43:43 -0600

    robot-name solution

--- go/robot-name/robot_name.go
+++ go/robot-name/robot_name.go
@@ -1,16 +1,44 @@
 package robotname
 
+import (
+	"bytes"
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+var letters = []rune{
+	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+	'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+	'W', 'X', 'Y', 'Z',
+}
+
 // Robot represents a robot.
 type Robot struct {
 	name string
 }
 
-// Name returns the name of robot r.
+// Init initializes the seed of rand.
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// Name returns the name of robot r; created if necessary.
 func (r *Robot) Name() string {
+	if r.name == "" {
+		var buf bytes.Buffer
+
+		buf.WriteRune(letters[rand.Intn(26)])
+		buf.WriteRune(letters[rand.Intn(26)])
+		buf.WriteString(strconv.Itoa(rand.Intn(900) + 100))
+
+		r.name = buf.String()
+	}
+
 	return r.name
 }
 
-// Reset gives robot r a new name, different from its previous name.
+// Reset deletes the name of robot r.
 func (r *Robot) Reset() {
-
+	r.name = ""
 }
