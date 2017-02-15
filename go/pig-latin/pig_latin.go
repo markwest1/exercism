04a917681e1956183853fad8321ee1fc568596bb
markwest1 <mark.west1@gmail.com>
2017-02-15 13:54:29 -0600

    pig-latin fetched, failing unit tests

---
+++ go/pig-latin/pig_latin.go
@@ -0,0 +1,13 @@
+package igpay
+
+// PigLatin translates an english word into pig-latin using the following
+// 1. If a word begins with a vowel sound, add "ay" to the end of the word.
+// 2. If a word begins with a consonant sound, move it to the end of the word,
+//    and then add an "ay" sound to the end of the word.
+func PigLatin(in string) string {
+	if in == "" {
+		return ""
+	}
+
+	return ""
+}
